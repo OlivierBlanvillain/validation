abf105c1dccd1e1b701fe136bd50d35531ca91d0
Olivier Blanvillain <olivier.blanvillain@gmail.com>
2015-09-15 00:05:06 +0200

    Compiles

--- validation-experimental/src/main/scala/Formatter.scala
+++ validation-experimental/src/main/scala/Formatter.scala
@@ -10,47 +10,52 @@ object show {
   /**
 
 trait RuleLike[I, O] {
-  def validate(data: I): Validated[Seq[(Path, Seq[ValidationError])], O]
+  def validate(data: I): Validated[Seq[(Path, Seq[VE])], O]
 }
 
    */
-
+  type VE = ValidationError
   type Rulz[T] = RuleLike[String, T]
 
-    object RuleLike {
-
-    implicit val stringR: Rulz[String] = ???
-    implicit val doubleR: Rulz[Double] = ???
-    implicit val intR: Rulz[Int] = ???
-
-    implicit def ruleGeneric[F, G]
-      (implicit
-        gen: LabelledGeneric.Aux[F, G],
-        sg: Lazy[Rulz[G]]
-      ): Rulz[F] =
-        new RuleLike[String, F] {
-          def validate(s: String): VA[F] = sg.value.validate(s).map(gen.from)
-        }
-
-    implicit def ruleHNil: Rulz[HNil] =
-      new Rulz[HNil] {
-        def validate(s: String): Validated[Seq[(Path, Seq[ValidationError])], HNil] = ???
-      }
-
-    implicit def ruleHCons[K <: Symbol, V, T <: HList]
-      (implicit
-        key: Witness.Aux[K],
-        sv: Lazy[Rulz[V]],
-        st: Lazy[Rulz[T]]
-      ): Rulz[FieldType[K, V] :: T] =
-        new Rule[String, FieldType[K, V] :: T] {
-          def validate(s: String): VA[FieldType[K, V] :: T] = {
-            ???
-          }
-        }
-
+  implicit val stringR: Rulz[String] = Rule.fromMapping { s => Valid(s) }
+  implicit val intR: Rulz[Int] = Rule.fromMapping { s =>
+    try {
+      Valid(s.toInt)
+    } catch {
+      case _: Exception => Valid(-1)
+    }
   }
 
+  implicit def ruleGeneric[F, G]
+    (implicit
+      gen: LabelledGeneric.Aux[F, G],
+      sg: Lazy[Rulz[G]]
+    ): Rulz[F] =
+      new RuleLike[String, F] {
+        def validate(s: String): Validated[Seq[(Path, Seq[VE])], F] =
+          sg.value.validate(s).map(gen.from)
+      }
+
+  implicit def ruleHNil: Rulz[HNil] =
+    new Rulz[HNil] {
+      def validate(s: String): Validated[Seq[(Path, Seq[VE])], HNil] = Valid(HNil)
+    }
+
+  implicit def ruleHCons[K <: Symbol, V, T <: HList]
+    (implicit
+      key: Witness.Aux[K],
+      sv: Lazy[Rulz[V]],
+      st: Lazy[Rulz[T]]
+    ): Rulz[FieldType[K, V] :: T] =
+      new Rule[String, FieldType[K, V] :: T] {
+        def validate(s: String): Validated[Seq[(Path, Seq[VE])], FieldType[K, V] :: T] = {
+          val svV = sv.value.validate(s)
+          val stV = st.value.validate(s)
+          val stKV = stV.map((t: T) => (v: V) => field[K](v) :: t)
+          svV.ap(stKV)
+        }
+      }
+      
   sealed trait Animal
   case class Cat(name: String, fish: Int) extends Animal
   case class Dog(name: String, bones: Int) extends Animal
@@ -58,5 +63,7 @@ trait RuleLike[I, O] {
 
 object main extends App {
   import show._
-  implicitly[Rulz[Cat]]
+  println(
+    implicitly[Rulz[Cat]].validate("")
+  )
 }
