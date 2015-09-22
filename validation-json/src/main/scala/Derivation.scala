71f69e66ff098c7d359382c939437efa727fdfcd
Olivier Blanvillain <olivier.blanvillain@gmail.com>
2015-09-22 08:50:12 +0200

    Syntax

--- validation-json/src/main/scala/Derivation.scala
+++ validation-json/src/main/scala/Derivation.scala
@@ -1,18 +1,16 @@
 package jto.validation
 package json
 
-import Rules._
-import Writes._
+import jto.validation.json.Rules._
+import jto.validation.json.Writes._
 import shapeless.{Path => _, _}
-import play.api.libs.json.{JsValue, JsObject, Json}
-import labelled._
-import cats.Monoid
-import shapeless.test.illTyped
 
-object TypePath {
-  val typePath = Path \ "$type"
-}
-import TypePath._
+import shapeless.labelled._
+import cats.Monoid
+
+import play.api.libs.json.{JsValue, JsObject, Json}
+
+// Derivation of RuleLike
 
 trait RuleProduct {
   implicit def ruleProductBaseCase[I](p: Path): RuleLike[I, HNil] =
@@ -39,6 +37,8 @@ trait RuleProduct {
 }
 
 trait RuleCoproduct {
+  def typePath: Path
+  
   implicit def ruleCoproductBaseCase[I](p: Path): RuleLike[I, CNil] =
     new RuleLike[I, CNil] {
       def validate(i: I): VA[CNil] =
@@ -79,6 +79,8 @@ trait RuleGeneric {
     }
 }
 
+// Derivation of WriteLike
+
 trait WriteProduct {
   type Output
   
@@ -107,6 +109,7 @@ trait WriteProduct {
 }
 
 trait WriteCoproduct {
+  def typePath: Path
   type Output
   
   implicit val writeCoproductBaseCase =
@@ -149,39 +152,78 @@ trait WriteGeneric {
     }
 }
 
-object DeriveJsonCoproduct
+// Syntax
+
+object DRule {
+  def derive[I, F, G]
+    (implicit
+      gen: LabelledGeneric.Aux[F, G],
+      sg: Lazy[Path => RuleLike[I, G]]
+    ): RuleLike[I, F] =
+      new RuleGeneric{}.ruleGeneric
+}
+
+object DWrite {
+  def derive[O, F, G]
+    (implicit
+      gen: LabelledGeneric.Aux[F, G],
+      sg: Lazy[Path => WriteLike[G, O]]
+    ): WriteLike[F, O] =
+      new WriteGeneric{}.writeGeneric
+}
+
+trait JsonStuff {
+  type Output = JsObject
+  val typePath = Path \ "$type"
+}
+
+object Derive
+  extends WriteProduct
+  with WriteCoproduct
+  with RuleProduct
+  with RuleCoproduct
+  with JsonStuff
+
+object DeriveReq
   extends WriteProduct
   with WriteCoproduct
   with WriteGeneric
   with RuleProduct
   with RuleCoproduct
   with RuleGeneric
-  { type Output = JsObject }
+  with JsonStuff
 
-object DeriveJsonProduct
+object DeriveProductReq
   extends WriteProduct
   with WriteGeneric
   with RuleProduct
   with RuleGeneric
-  { type Output = JsObject }
+  with JsonStuff
 
-sealed trait Animal
-case class Cat(name: String, fish: Int, friend: Dog) extends Animal
-case class Dog(name: String, bones: Int) extends Animal
+object Test extends App {
+  import shapeless.test.illTyped
+  
+  def test[A](arg1: A, arg2: A): Unit =
+    if(arg1 == arg2)
+      println(arg1)
+    else
+      throw new Exception(s"""
+      |  arg1: $arg1
+      |  arg2: $arg2""".stripMargin)
+  
+  def test[A, AA <: A, T](r: RuleLike[A, T], w: WriteLike[T, AA])(v: T): Unit =
+    test(Some(v), r.validate(w.writes(v)).toOption)
+  
+  def test[I, O](r1: RuleLike[I, O], r2: RuleLike[I, O])(i: I): Unit =
+    test(r1.validate(i), r2.validate(i))
 
-case class Dogception(animal: Animal, dogception: Option[Dogception])
-
-object main extends App {
-  val catJson = Json.parse("""
-    {
-      "name": "miam",
-      "fish": 3,
-      "friend": {
-        "name": "doge",
-        "bones": 0
-      }
-    }
-  """)
+  def test[I, O](w1: WriteLike[I, O], w2: WriteLike[I, O])(i: I): Unit =
+    test(w1.writes(i), w2.writes(i))
+  
+  sealed trait Animal
+  case class Cat(name: String, fish: Int, friend: Dog) extends Animal
+  case class Dog(name: String, bones: Int) extends Animal
+  case class WithOptions(i: Int, os: Option[String])
   
   val dogJson = Json.parse("""
     {
@@ -189,95 +231,79 @@ object main extends App {
       "bones": 0
     }
   """).as[JsObject]
-
-  val dogceptionJson = Json.parse("""
-    {
-      "animal": {
-        "$type": "Dog",
-        "name": "doge",
-        "bones": 0
-      },
-      "dogception": {
-        "$type": "Some",
-        "x": {
-          "animal": {
-            "$type": "Dog",
-            "name": "nested doge",
-            "bones": 1
-          },
-          "dogception": {
-            "$type": "None"
-          }
-        }
-      }
-    }
-  """).as[JsObject]
   
   val dog = Dog("doge", 0)
   val cat = Cat("miaou", 1, dog)
   
-  def test[A](arg1: A, arg2: A): Unit = {
-    if(arg1 == arg2)
-      println(arg1)
-    else
-      throw new Exception(s"""
-      |  arg1: $arg1
-      |  arg2: $arg2""".stripMargin)
-  }
-  
-  def testFormat[A, AA <: A, T](r: RuleLike[A, T], w: WriteLike[T, AA])(v: T): Unit = {
-    test(Some(v), r.validate(w.writes(v)).toOption)
-  }
-  
   "Derivated instance for Dog behaves the same as macro generated ones"; {
-    val (genRuleJsValue2Dog, genRuleJsObject2Dog, genWriteDog2JsObject) = (
-      Rule.gen[JsValue, Dog],
+    import Derive._
+    
+    test(
       Rule.gen[JsObject, Dog],
-      Write.gen[Dog, JsObject]
-    )
+      DRule.derive: RuleLike[JsObject, Dog]
+    )(dogJson)
+
+    test(
+      Rule.gen[JsValue, Dog],
+      DRule.derive: RuleLike[JsValue, Dog]
+    )(dogJson)
     
-    val (derRuleJsValue2Dog, derRuleJsObject2Dog, derWriteDog2JsObject) = {
-      import DeriveJsonCoproduct._
-      (
-        implicitly[RuleLike[JsObject, Dog]],
-        implicitly[RuleLike[JsValue, Dog]],
-        implicitly[WriteLike[Dog, JsObject]]
-      )
-    }
+    test(
+      Write.gen[Dog, JsObject],
+      DWrite.derive: WriteLike[Dog, JsObject]
+    )(dog)
+  }
+  
+  "Non recursive derivation fails when generation with macro fails"; {
+    import Derive._
     
-    test(genRuleJsValue2Dog.validate(dogJson), derRuleJsValue2Dog.validate(dogJson))
-    test(genRuleJsObject2Dog.validate(dogJson), derRuleJsObject2Dog.validate(dogJson))
-    test(genWriteDog2JsObject.writes(dog), derWriteDog2JsObject.writes(dog))
+    illTyped("Rule.gen[JsValue, Cat]")
+    illTyped("DRule.derive: RuleLike[JsValue, Cat]")
+    
+    illTyped("Write.gen[Cat, JsObject]")
+    illTyped("DWrite.derive: WriteLike[Cat, JsObject]")
+
+    illTyped("Rule.gen[JsValue, Animal]")
+    illTyped("DRule.derive: RuleLike[JsValue, Animal]")
+    
+    illTyped("Write.gen[Animal, JsObject]")
+    illTyped("DWrite.derive: WriteLike[Animal, JsObject]")
   }
   
   "Recursive product derivation handles nested case classes"; {
-    import DeriveJsonCoproduct._
-    illTyped("Rule.gen[JsValue, Cat]")
-    illTyped("Write.gen[Cat, JsObject]")
+    import DeriveReq._
     
-    val r = implicitly[RuleLike[JsValue, Cat]]
-    val w = implicitly[WriteLike[Cat, JsObject]]
-    
-    testFormat(r, w)(cat)
+    test(
+      implicitly[RuleLike[JsValue, Cat]],
+      implicitly[WriteLike[Cat, JsObject]]
+    )(cat)
   }
-  
+
   "Coproduct derivation handles ADTs"; {
-    import DeriveJsonCoproduct._
+    import DeriveReq._
     
     illTyped("Rule.gen[JsValue, Animal]")
     illTyped("Write.gen[Animal, JsObject]")
     
-    val r = implicitly[RuleLike[JsValue, Animal]]
-    val w = implicitly[WriteLike[Animal, JsObject]]
-    
-    List(dog, cat) foreach testFormat(r, w)
+    List(dog, cat) foreach test(
+      implicitly[RuleLike[JsValue, Animal]],
+      implicitly[WriteLike[Animal, JsObject]])
   }
   
   "Derivation instance with Option behaves the same as macro generated ones"; {
-    case class WithOptions(i: Int, os: Option[String])
-    import DeriveJsonProduct._
+    import Derive._
+    
     val wo = WithOptions(1, Some("ksjdf"))
-    val json = implicitly[WriteLike[WithOptions, JsObject]].writes(wo)
-    test(wo, implicitly[RuleLike[JsValue, WithOptions]].validate(json).toOption.get)
+    val woJson = Json.parse("""{"i": 1, "os": "ksjdf"}""")
+    val woNone = WithOptions(1, None)
+    val woNoneJson = Json.parse("""{"i": 1}""")
+    
+    List(wo, woNone) foreach test(
+      Write.gen[WithOptions, JsObject],
+      DWrite.derive: WriteLike[WithOptions, JsObject])
+    
+    List(woJson, woNoneJson) foreach test(
+      Rule.gen[JsValue, WithOptions],
+      DRule.derive: RuleLike[JsValue, WithOptions])
   }
 }
