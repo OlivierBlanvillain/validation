024a2e2c96e4df85d49a49af3d915f55e458d7f7
Olivier Blanvillain <olivier.blanvillain@gmail.com>
2015-09-23 08:45:19 +0200

    Polish tests

--- validation-json/src/test/scala/DerivationSpec.scala
+++ validation-json/src/test/scala/DerivationSpec.scala
@@ -1,50 +1,46 @@
 import jto.validation._
 import jto.validation.json._
-import jto.validation.json.Rules._
-import jto.validation.json.Writes._
 import play.api.libs.json.{JsValue, JsObject, Json}
 import DerivationTestLib._
-import shapeless.test.illTyped
+import org.specs2.mutable._
+import org.specs2.execute.Result
 
-object Test extends App {
+object DerivationSpec extends Specification {
+  
+  implicit def iDontLikeSpec2[T](t: T): Result = Result.unit(())
   
   sealed trait Animal
-  case class Cat(name: String, fish: Int, friend: Dog) extends Animal
   case class Dog(name: String, bones: Int) extends Animal
-  case class WithOptions(i: Int, os: Option[String])
-  case class WithList(i: Int, ls: List[String])
+  case class Cat(name: String, fish: Int, friend: Option[Cat]) extends Animal
   
-  val dogJson = Json.parse("""
-    {
-      "name": "doge",
-      "bones": 0
-    }
-  """).as[JsObject]
+  case class WithOptions(os: Option[String])
+  case class WithList(ls: List[String])
   
-  val dog = Dog("doge", 0)
-  val cat = Cat("miaou", 1, dog)
-    
-  "Derivated instance for Dog behaves the same as macro generated ones"; {
+  "Derivation syntax should be similar to the macro syntax" in {
+    import Rules._, Writes._
     import Derivation._
     
-    test(
-      Rule.gen[JsObject, Dog],
-      Rule.derive: RuleLike[JsObject, Dog]
-    )(dogJson)
+    val derivedRuleJsObject: RuleLike[JsObject, Dog] = Rule.derive
+    val macroRuleJsObejct: RuleLike[JsObject, Dog] = Rule.gen[JsObject, Dog]
 
-    test(
-      Rule.gen[JsValue, Dog],
-      Rule.derive: RuleLike[JsValue, Dog]
-    )(dogJson)
+    val derivedRuleJsValue: RuleLike[JsValue, Dog] = Rule.derive
+    val macroRuleJsValue: RuleLike[JsValue, Dog] = Rule.gen[JsValue, Dog]
+
+    val derivedWriteJsObject: WriteLike[Dog, JsObject] = Write.derive
+    val macroWriteJsObject: WriteLike[Dog, JsObject] = Write.gen[Dog, JsObject]
     
-    test(
-      Write.gen[Dog, JsObject],
-      Write.derive: WriteLike[Dog, JsObject]
-    )(dog)
+    val dogJson = Json.parse("""{"name": "doge", "bones": 0}""").as[JsObject]
+    val dog = Dog("doge", 0)
+    
+    derivedRuleJsObject.validate(dogJson) mustEqual macroRuleJsObejct.validate(dogJson)
+    derivedRuleJsValue.validate(dogJson) mustEqual macroRuleJsValue.validate(dogJson)
+    derivedWriteJsObject.writes(dog) mustEqual macroWriteJsObject.writes(dog)
   }
   
-  "Non recursive derivation fails when generation with macro fails"; {
+  "Non recursive derivation fails when macro generation fails" in {
+    import Rules._, Writes._
     import Derivation._
+    import shapeless.test.illTyped
     
     illTyped("Rule.gen[JsValue, Cat]")
     illTyped("Rule.derive: RuleLike[JsValue, Cat]")
@@ -59,47 +55,52 @@ object Test extends App {
     illTyped("Write.derive: WriteLike[Animal, JsObject]")
   }
   
-  "Recursive product derivation handles nested case classes"; {
+  "Coproduct derivation handles ADTs" in {
+    import Rules._, Writes._
     import DerivationReq._
     
-    test(
-      implicitly[RuleLike[JsValue, Cat]],
-      implicitly[WriteLike[Cat, JsObject]]
-    )(cat)
-  }
-
-  "Coproduct derivation handles ADTs"; {
-    import DerivationReq._
+    sealed trait ADT
+    case object A extends ADT
+    case object D extends ADT
+    case object T extends ADT
     
-    illTyped("Rule.gen[JsValue, Animal]")
-    illTyped("Write.gen[Animal, JsObject]")
+    val a = A
+    val aJson = Json.parse("""{"$type":"A"}""")
     
-    List(dog, cat) foreach test(
-      implicitly[RuleLike[JsValue, Animal]],
-      implicitly[WriteLike[Animal, JsObject]])
-  }
-
-  "Derivation instance with Option behaves the same as macro generated ones"; {
-    import DerivationReq._
-    
-    val wo = WithOptions(1, Some("ksjdf"))
-    val woJson = Json.parse("""{"i": 1, "os": "ksjdf"}""")
-    val woNone = WithOptions(1, None)
-    val woNoneJson = Json.parse("""{"i": 1}""")
-    
-    List(wo, woNone) foreach test(
-      Write.gen[WithOptions, JsObject],
-      implicitly[WriteLike[WithOptions, JsObject]])
-    
-    List(woJson, woNoneJson) foreach test(
-      Rule.gen[JsValue, WithOptions],
-      implicitly[RuleLike[JsValue, WithOptions]])
-    
-    implicitly[WriteLike[WithList, JsObject]]
-    implicitly[RuleLike[JsValue, WithList]]
+    implicitly[WriteLike[ADT, JsObject]].writes(a) mustEqual aJson
+    implicitly[RuleLike[JsValue, ADT]].validate(aJson) mustEqual Valid(a)
   }
   
-  {
+  "Recursive derivation handles recursive case classes" in {
+    import Rules._, Writes._
+    import DerivationReq._
+    
+    val cat = Cat("le chat", 1, Some(Cat("garfield", 0, None)))
+    val catJson = Json.parse("""{"name":"le chat","fish":1,"friend":{"name":"garfield","fish":0}}""")
+
+    implicitly[WriteLike[Cat, JsObject]].writes(cat) mustEqual catJson
+    implicitly[RuleLike[JsValue, Cat]].validate(catJson) mustEqual Valid(cat)
+  }
+
+  "Derivation instance with Option behaves the same as macro generated ones" in {
+    import Rules._, Writes._
+    import DerivationReq._
+    
+    val woSome = WithOptions(Some("ksjdf"))
+    val woSomeJson = Json.parse("""{"os": "ksjdf"}""")
+    
+    val woNone = WithOptions(None)
+    val woNoneJson = Json.parse("{}")
+    
+    Write.gen[WithOptions, JsObject].writes(woSome) mustEqual implicitly[WriteLike[WithOptions, JsObject]].writes(woSome)
+    Write.gen[WithOptions, JsObject].writes(woNone) mustEqual implicitly[WriteLike[WithOptions, JsObject]].writes(woNone)
+
+    Rule.gen[JsValue, WithOptions].validate(woSomeJson) mustEqual implicitly[RuleLike[JsValue, WithOptions]].validate(woSomeJson)
+    Rule.gen[JsValue, WithOptions].validate(woNoneJson) mustEqual implicitly[RuleLike[JsValue, WithOptions]].validate(woNoneJson)
+  }
+  
+  "Moar tests!!!" in { 
+    import Rules._, Writes._
     import DerivationReq._
     testAgainstMacro(Format.gen[JsValue, JsObject, Dog])
     testAgainstItself[JsValue, JsObject, Cat]
